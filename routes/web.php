534c51d20f0ca8aeb517fc719afe66ef1ca3d388
Luong Duy <nhansatthuas@gmail.com>
2017-04-17 21:51:05 +0700

    study
    study

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,24 @@ Route::get('test',function(){
   echo "được phết này";
 });
 Route::get('test2','HomeController@testview');
+Route::get('thong-tin/{hoten}/{sdt}', function($hoten,$sdt){
+  return "Thông tin của bạn là: $hoten với số điện thoại là: $sdt ";
+})->where(['sdt' => '[0-9]{10,11}', 'hoten' => '[a-zA-Z]+']);
+Route::get('show-view',function(){
+  $ten= "duy";
+  $view='Admin';
+  return view('test',compact('ten','view'));
+});
+Route::get('test-controller','HomeController@testAction');
+Route::get('ho-chi-minh',['as'=>'hcm',function(){
+  return "Hồ Chí Minh đẹp lắm các bạn ơi";
+}]);
+route::get('mon-an/bun-bo',function(){
+  echo "Đây là trang bán bún bò";
+});
+route::get('mon-an/bun-mam',function(){
+  echo "Đây là trang bán bún mắm";
+});
+route::get('mon-an/bun-moc',function(){
+  echo "Đây là trang bán bún mọc";
+});
