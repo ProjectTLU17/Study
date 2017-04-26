0787f784da398e4b50e9d605425b4265137a40c5
Luong Duy <nhansatthuas@gmail.com>
2017-04-26 22:27:35 +0700

    update 5
    setting and migration,seeding for user table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call('UsersTableSeeder');
+    }
+}
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->insert([
+          ['name'=>'Lương Đức Duy','username'=>'duyluong1994','password'=>'12345','role'=>'admin'],
+          ['name'=>'Vũ Đàm Khánh','username'=>'khanhvudam1995','password'=>'12345','role'=>'manager'],
+          ['name'=>'Nguyễn Đình Phong','username'=>'phongnguyen1995','password'=>'12345','role'=>'admin'],
+          ['name'=>'Nguyễn Duy Hoang Anh','username'=>'hoanganh1995','password'=>'12345','role'=>'manager'],
+          ['name'=>'Đặng Trung Kiên','username'=>'kien1995','password'=>'12345','role'=>'employee'],
+          ['name'=>'Trần Sơn Tùng','username'=>'tung1995','password'=>'12345','role'=>'employee']
+        ]);
     }
 }
