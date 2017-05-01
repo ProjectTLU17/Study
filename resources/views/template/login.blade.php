1efb3a420ea42a07dc2afa6b93217b613eef02fb
Pogg <fogkio@gmail.com>
2017-05-01 18:57:43 +0700

    Form login

--- resources/views/template/login.blade.php
+++ resources/views/template/login.blade.php
@@ -1,47 +1,33 @@
-@extends('template.layouts')
-@section('title','Đăng Nhập')
-@section('header')
-@endsection
-@section('main')
-    <div class="container" style="margin-top:40px;">
-      {!! Html::ul($errors->all(), array('class'=>'alert alert-danger errors')) !!}
-    {!!Form::open(['route'=>'login'])!!}
-      {!!Form::token()!!}
-	<div class="row">
-		<div class="col-sm-6 col-md-4 col-md-offset-4">
-			<div class="panel panel-default panel-frm">
-				<div class="panel-heading panel-heading-frm">
-					<strong> HOME LAND COPORATION </strong>
-				</div>
-				<div class="panel-body">
-						<fieldset>
-							<div class="row">
-								<div class="col-sm-12 col-md-10  col-md-offset-1 ">
-									<div class="form-group">
-										<div class="input-group">
-											<span class="input-group-addon">
-												<i class="glyphicon glyphicon-user"></i>
-											</span>
-											<input name="username" value=""  placeholder="Tài khoản" class="form-control form-frm-input" type="text">
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="input-group">
-											<span class="input-group-addon">
-												<i class="glyphicon glyphicon-lock"></i>
-											</span>
-											<input name="password" placeholder="Mật khẩu" class="form-control form-frm-input" type="password">
-										</div>
-									</div>
-										<div class="form-group">
-										<input class="btn btn-lg btn-primary btn-block" value="Sign in" type="submit">
-									</div>
-								</div>
-							</div>
-						</fieldset>
-				</div>
-      </div>
-		</div>
-	 </div>
-	{!!Form::close() !!}
-</div>
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel="stylesheet" href="css/loginstyle.css">
+    <link rel="shortcut icon" href="/img/favicon.png" />
+  </head>
+  <body>
+    <div class="container">
+    <div class="login">
+    	<h1 class="login-heading">
+        <strong>Welcome.</strong> Please login.</h1>
+        <div class="errors">
+          {{$errors->first('username')}}
+          {{$errors->first('password')}}
+        </div>
+          {!!Form::open(['route'=>'login'])!!}
+          {!!Form::token()!!}
+          <input type="text" name="username" placeholder="Username" required="required" class="input-txt" />
+            <input type="password" name="password" placeholder="Password" required="required" class="input-txt" />
+            <div class="login-footer">
+               <a href="#" class="lnk">
+                <span class="icon icon--min">ಠ╭╮ಠ</span>
+                I've forgotten something</a>
+              <button type="submit" class="btn btn--right">Sign in  </button>
+            </div>
+	        {!!Form::close() !!}
+    </div>
+  </div>
+  </body>
+</html>
