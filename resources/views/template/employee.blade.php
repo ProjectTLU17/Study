65840a45e48ca8a20824897a270a0423d06af8f2
Luong Duy <nhansatthuas@gmail.com>
2017-05-03 00:00:33 +0700

    update for master
    5/3 workday

--- resources/views/template/employee.blade.php
+++ resources/views/template/employee.blade.php
@@ -1,5 +1,8 @@
 @extends('template.layouts')
 @section('title','Nhân viên')
 @section('main')
+  @if(Auth::user()->role=="manager")
+    <a href="/manager/users">Quản lý tài khoản</a>
+@endif
   <a type="button" href="/logout">Logout</a>
 @endsection
