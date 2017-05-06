5469448920a153ae9c8d04c9646a5685de2bf549
Luong Duy <nhansatthuas@gmail.com>
2017-05-07 01:59:10 +0700

    1

--- resources/views/dashbroad/product-index.blade.php
+++ resources/views/dashbroad/product-index.blade.php
@@ -6,13 +6,17 @@
         <div class="col-lg-12">
             <h1 class="page-header">Biệt thự</h1>
         </div>
-        <div class="row">
+
         @foreach ($product as $item)
           <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+            <div class="row">
               <a class="thumbnail"><img class="img-responsive" src="/upload/images/{!!$item->images!!}" alt="404"></a>
-              <a type="button" class="btn btn-danger pull-right" href="{!!route('product.destroy', $item->id)!!}">Xóa</a>
-              <a type="button" class="btn btn-info pull-left">Xem</a>
-              <a type="button" class="btn btn-success pull-center">Sửa</a>
+            </div>
+            <div class="row">
+              <a type="button" class="btn btn-info col-md-3 ">Xem</a>
+              <a type="button" class="btn btn-success col-md-3">Sửa</a>
+              <a type="button" class="btn btn-danger col-md-3" href="{!!route('product.destroy', $item->id)!!}">Xóa</a>
+            </div>
           </div>
           @endforeach
         </div>
