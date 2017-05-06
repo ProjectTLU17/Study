385c9cffc1449476c4fadc86c66c6d4d3aa34bb4
Pogg <fogkio@gmail.com>
2017-05-06 22:25:46 +0700

    update

--- resources/views/manager/user-details.blade.php
+++ resources/views/manager/user-details.blade.php
@@ -1,4 +1,51 @@
 @extends('template.menubar-employee')
 @section('title','Thông tin cá nhân')
 @section('main')
+<div class="container">
+  <h2 class="text-center">Thông tin cá nhân</h2>
+  <form class="form-horizontal" action="/action_page.php">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Họ và tên: </label>
+      <div class="col-sm-10">
+        <input class="form-control" value="{!!Auth::user()->name!!}" disabled>
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Tên đăng nhập: </label>
+      <div class="col-sm-10">
+        <input class="form-control" value="{!!Auth::user()->username!!}" disabled>
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Job Role: </label>
+      <div class="col-sm-10">
+        <input class="form-control" value="{!!Auth::user()->role!!}" disabled>
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Birthday: </label>
+      <div class="col-sm-10">
+        <input class="form-control" value="{!!Auth::user()->birthday!!}" disabled>
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Address: </label>
+      <div class="col-sm-10">
+        <input class="form-control" value="{!!Auth::user()->address!!}" disabled>
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Telephone: </label>
+      <div class="col-sm-10">
+        <input class="form-control" value="{!!Auth::user()->phone!!}" disabled>
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Email: </label>
+      <div class="col-sm-10">
+        <input class="form-control" value="{!!Auth::user()->email!!}" disabled>
+      </div>
+    </div>
+  </form>
+</div>
 @stop
