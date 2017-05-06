024d9ea048d99aa22bf49862c0ecbfab165adee8
Pogg <fogkio@gmail.com>
2017-05-07 06:10:45 +0700

    update

--- resources/views/template/manager.blade.php
+++ resources/views/template/manager.blade.php
@@ -257,13 +257,13 @@
           </div>
 
   <!-- jQuery -->
-  <script src="js/jquery.js"></script>
+  <script src="{{asset('js/jquery.js')}}"></script>
 
   <!-- Bootstrap Core JavaScript -->
-  <script src="js/bootstrap.min.js"></script>
+  <script src="{{asset('js/bootstrap.min.js')}}"></script>
 
   <!-- gzris Charts JavaScript -->
-  <script src="js/plugins/morris/raphael.min.js"></script>
-  <script src="js/plugins/morris/morris.min.js"></script>
-  <script src="js/plugins/morris/morris-data.js"></script>
+  <script src="{{asset('js/plugins/morris/raphael.min.js')}}"></script>
+  <script src="{{asset('js/plugins/morris/morris.min.js')}}"></script>
+  <script src="{{asset('js/plugins/morris/morris-data.js')}}"></script>
 @stop
