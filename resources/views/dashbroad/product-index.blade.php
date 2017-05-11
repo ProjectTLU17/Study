b1ef4e93db1e3d813976168b86e599ab08b57d3b
Pogg <fogkio@gmail.com>
2017-05-11 14:49:28 +0700

    update

--- resources/views/dashbroad/product-index.blade.php
+++ resources/views/dashbroad/product-index.blade.php
@@ -18,5 +18,52 @@
           </div>
           @endforeach
         </div>
-      </div>
+        <div class="row">
+          <div class="col-lg-12">
+              <h1 class="page-header">Căn hộ<a href="#" type="button" class="btn btn-warning pull-right">Thêm mới</a></h1>
+          </div>
+
+          @foreach ($product as $item)
+            <div class="col-lg-3 col-md-3 col-xs-6 thumb text-center">
+              <div class="row">
+                <a class="thumbnail"><img class="img-responsive" src="/upload/images/{!!$item->images!!}" alt="404"></a>
+                <a type="button" class="btn btn-info">Xem</a>
+                <a type="button" class="btn btn-success">Sửa</a>
+                <a type="button" class="btn btn-danger" href="{!!route('product.destroy', $item->id)!!}">Xóa</a>
+              </div>
+            </div>
+            @endforeach
+          </div>
+          <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Nhà liền kề <a href="#" type="button" class="btn btn-warning pull-right">Thêm mới</a></h1>
+            </div>
+
+            @foreach ($product as $item)
+              <div class="col-lg-3 col-md-3 col-xs-6 thumb text-center">
+                <div class="row">
+                  <a class="thumbnail"><img class="img-responsive" src="/upload/images/{!!$item->images!!}" alt="404"></a>
+                  <a type="button" class="btn btn-info">Xem</a>
+                  <a type="button" class="btn btn-success">Sửa</a>
+                  <a type="button" class="btn btn-danger" href="{!!route('product.destroy', $item->id)!!}">Xóa</a>
+                </div>
+              </div>
+              @endforeach
+            </div>
+            <div class="row">
+              <div class="col-lg-12">
+                  <h1 class="page-header">Lô đất <a href="#" type="button" class="btn btn-warning pull-right">Thêm mới</a></h1>
+              </div>
+
+              @foreach ($product as $item)
+                <div class="col-lg-3 col-md-3 col-xs-6 thumb text-center">
+                  <div class="row">
+                    <a class="thumbnail"><img class="img-responsive" src="/upload/images/{!!$item->images!!}" alt="404"></a>
+                    <a type="button" class="btn btn-info">Xem</a>
+                    <a type="button" class="btn btn-success">Sửa</a>
+                    <a type="button" class="btn btn-danger" href="{!!route('product.destroy', $item->id)!!}">Xóa</a>
+                  </div>
+                </div>
+                @endforeach
+              </div>
 @endsection
