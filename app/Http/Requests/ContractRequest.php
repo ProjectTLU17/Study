1a22c82556f4143f63073dc0c93cb083b4e30cd9
Luong Duy <nhansatthuas@gmail.com>
2017-05-12 03:13:32 +0700

    update

--- app/Http/Requests/ContractRequest.php
+++ app/Http/Requests/ContractRequest.php
@@ -26,10 +26,10 @@ class ContractRequest extends FormRequest
       switch ($this->method()) {
         case 'POST':{
           return [
-            'cus_id'=>'required|exists:customer,id',
-          'emp_id'=>'required|exists:employee,id',
-          'prodt_id'=>'required|exists:product,id',
-          'details'=>'required',
+          'customer_id'=>'required|exists:customer,id',
+          'users_id'=>'required|exists:employee,id',
+          'product_id'=>'required|exists:product,id',
+          'decription'=>'required',
           'startdate'=>'required|date',
           'expdate'=>'date|nullable',
           'status'=>'required',
@@ -37,10 +37,10 @@ class ContractRequest extends FormRequest
         }
         case 'PUT':{
           return [
-            'cus_id'=>'required|exists:customer,id',
-            'emp_id'=>'required|exists:employee,id',
-            'prodt_id'=>'required|exists:product,id',
-            'details'=>'required',
+            'customer_id'=>'required|exists:customer,id',
+            'users_id'=>'required|exists:employee,id',
+            'product_id'=>'required|exists:product,id',
+            'decription'=>'required',
             'startdate'=>'required|date',
             'expdate'=>'date|nullable',
             'status'=>'required',
@@ -53,13 +53,13 @@ class ContractRequest extends FormRequest
     }
     public function messages(){
       return [
-        'cus_id.required'=>'Chưa chọn khách hàng',
-        'cus_id.exists'=>'ID không có trong Cơ sở dữ liệu',
-        'emp_id.required'=>'Chưa chọn nhân viên xử lí',
-        'emp_id.exists'=>'ID không có trong Cơ sở dữ liệu',
-        'prodt_id.required'=>'Chưa chọn sản phẩm',
-        'prodt_id.exists'=>'ID không có trong Cơ sở dữ liệu',
-        'details.required'=>'Vui lòng nhập thông tin mô tả',
+        'customer_id.required'=>'Chưa chọn khách hàng',
+        'customer_id.exists'=>'ID không có trong Cơ sở dữ liệu',
+        'users_id.required'=>'Chưa chọn nhân viên xử lí',
+        'users_id.exists'=>'ID không có trong Cơ sở dữ liệu',
+        'product_id.required'=>'Chưa chọn sản phẩm',
+        'product_id.exists'=>'ID không có trong Cơ sở dữ liệu',
+        'decription.required'=>'Vui lòng nhập thông tin mô tả',
         'startdate.required'=>'Vui lòng nhập ngày bắt đầu hợp đồng',
         'startdate.date'=> 'Định dạng ngày phải là YYYY/MM/DD',
         'expdate.date'=> 'Định dạng ngày phải là YYYY/MM/DD',
