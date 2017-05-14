4b73d0ed724021d81e3c260768491cb1d3726a2a
Pogg <fogkio@gmail.com>
2017-05-14 09:24:13 +0700

    update

--- resources/views/dashbroad/product-index.blade.php
+++ resources/views/dashbroad/product-index.blade.php
@@ -3,9 +3,10 @@
 @section('main')
   <!-- Page Content -->
 
-        <div class="col-lg-12">
-            <a href="{!!route('product.create')!!}" type="button" class="btn btn-warning pull-right">Thêm mới</a>
-        </div>
+      <div class="col-lg-12">
+          <a href="{!!route('product.create')!!}" type="button" class="btn btn-warning pull-right">Thêm mới</a>
+      </div>
+
         @foreach ($category as $item_cate)
           <div class="row">
             @if (!$item_cate->product->isEmpty())
@@ -13,8 +14,8 @@
               @foreach ($item_cate->product as $prodt)
                 <div class="col-lg-3 col-md-3 col-xs-6 thumb text-center">
                   <div class="row">
-                    <a class="thumbnail" href="{!!route('product.show',$prodt->id)!!}"><img class="img-responsive" src="/upload/images/{!!$prodt->images->first()->name or '#'!!}" alt="404"></a>
-                    <a href="{!!route('product.show',$prodt->id)!!}"><h3>{!!$prodt->name!!}</h3></a>
+                    <a href="{!!route('product.show',$prodt->id)!!}"><img class="thumbnails" src="/upload/images/{!!$prodt->images->first()->name or '#'!!}" alt="404"></a>
+                    <a style="text-decoration:none;" href="{!!route('product.show',$prodt->id)!!}"><h3>{!!$prodt->name!!}</h3></a>
                     {{-- <a type="button" class="btn btn-success">Sửa</a> --}}
                     {!!Form::open(array('route'=>array('product.destroy',$prodt->id),'method'=>'DELETE'))!!}
                       <button type="submit" class="btn btn-danger" onclick="return xacnhanxoa('Bạn Có Chắc Muốn Xóa Không')">Xóa</button>
