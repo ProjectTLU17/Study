8a5afb08b51383f1fbebe3b0158c425b59c68f3e
Luong Duy <nhansatthuas@gmail.com>
2017-05-14 10:42:33 +0700

    Merge remote-tracking branch 'refs/remotes/origin/Front-End' into Back-End
    # Conflicts:
    #	resources/views/dashbroad/product-index.blade.php

--- resources/views/dashbroad/product-index.blade.php
+++ resources/views/dashbroad/product-index.blade.php
@@ -12,8 +12,8 @@
               @foreach ($item_cate->product as $prodt)
                 <div class="col-lg-3 col-md-3 col-xs-6 thumb text-center">
                   <div class="row">
-                    <a class="thumbnail" href="{!!route('product.show',$prodt->id)!!}"><img class="img-responsive" src="/upload/images/{!!$prodt->images->first()->name or '#'!!}" alt="404"></a>
-                    <a href="{!!route('product.show',$prodt->id)!!}"><h3>{!!$prodt->name!!}</h3></a>
+                    <a href="{!!route('product.show',$prodt->id)!!}"><img class="thumbnails" src="/upload/images/{!!$prodt->images->first()->name or '#'!!}" alt="404"></a>
+                    <a style="text-decoration:none;" href="{!!route('product.show',$prodt->id)!!}"><h3>{!!$prodt->name!!}</h3></a>
                     {{-- <a type="button" class="btn btn-success">Sửa</a> --}}
                     {!!Form::open(array('route'=>array('product.destroy',$prodt->id),'method'=>'DELETE'))!!}
                       <button type="submit" class="btn btn-danger" onclick="return xacnhanxoa('Bạn Có Chắc Muốn Xóa Không')">Xóa</button>
