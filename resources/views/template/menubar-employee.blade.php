a7045e292af329aceb4723932420f06662acacd3
Pogg <fogkio@gmail.com>
2017-06-01 23:08:49 +0700

    update

--- resources/views/template/menubar-employee.blade.php
+++ resources/views/template/menubar-employee.blade.php
@@ -77,6 +77,9 @@
       </div>
     </header>
     <!-- END .header -->
+
+
+
     <div class="container">
       @yield('main')
     </div>
@@ -114,6 +117,39 @@
                   }
                   return false;
               }
+          </script>
+          <script>
+            $(function(){
+              $(".panel.panel-chat > .panel-heading > .chatMinimize").click(function(){
+                  if($(this).parent().parent().hasClass('mini'))
+                  {
+                      $(this).parent().parent().removeClass('mini').addClass('normal');
+
+                      $('.panel.panel-chat > .panel-body').animate({height: "250px"}, 500).show();
+
+                      $('.panel.panel-chat > .panel-footer').animate({height: "75px"}, 500).show();
+                  }
+                  else
+                  {
+                      $(this).parent().parent().removeClass('normal').addClass('mini');
+
+                      $('.panel.panel-chat > .panel-body').animate({height: "0"}, 500);
+
+                      $('.panel.panel-chat > .panel-footer').animate({height: "0"}, 500);
+
+                      setTimeout(function() {
+                          $('.panel.panel-chat > .panel-body').hide();
+                          $('.panel.panel-chat > .panel-footer').hide();
+                      }, 500);
+
+
+                  }
+
+              });
+              $(".panel.panel-chat > .panel-heading > .chatClose").click(function(){
+                  $(this).parent().parent().remove();
+              });
+          })
           </script>
   </body>
 </html>
