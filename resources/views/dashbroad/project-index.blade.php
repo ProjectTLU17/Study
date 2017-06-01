8985e53b3adb3d474f0981c5ae2efce3c2277209
Luong Duy <nhansatthuas@gmail.com>
2017-06-01 23:57:55 +0700

    update

--- resources/views/dashbroad/project-index.blade.php
+++ resources/views/dashbroad/project-index.blade.php
@@ -79,7 +79,6 @@
           </div>
           <div class="form-group">
             <div class="col-sm-12">
-              <a type="button" class="btn btn-warning pull-right" href="{{ URL::previous() }}">Hủy bỏ</a>
               <button class="btn btn-info pull-right" type="submit">Thêm mới</button>
             </div>
           </div>
